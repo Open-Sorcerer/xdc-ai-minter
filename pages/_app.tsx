669257380860ee1a259636cfd8223c44756ef491
Saviour1001 <harshghodkar@gmail.com>
2023-10-06 20:48:25 +0530

    added the smart contracts

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { WagmiConfig, createConfig } from "wagmi";
+import { xdc } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import Layout from "../components/Layout";
+import ConnectKitTheme from "../components/ConnectKitTheme";
+
+const config = createConfig(
+  getDefaultConfig({
+    appName: "CrossChainCanvas",
+    chains: [xdc],
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    autoConnect: false,
+  })
+);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider
+        mode="light"
+        customTheme={ConnectKitTheme}
+        options={{
+          embedGoogleFonts: true,
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
